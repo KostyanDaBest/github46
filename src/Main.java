6409d4fa7ecefb68f901638ced28b9490c19f527
kosty <round.mc@yandex.by>
2023-11-16 12:56:58 -0500

    domashka

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Main {
-}
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int arr[] = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(10);
+        }
+        int i = 0;
+            for (i = 1; i < arr.length; i++) {
+                if (arr[i] <= arr[i - 1]) {
+                    System.out.println("Массив не является возрастающей последовательностью." + Arrays.toString(arr));
+                    break;
+                }
+                else if (arr[i] == arr.length) ;
+                {
+                    System.out.println(" Массив не является возрастающей последовательностью. "+ Arrays.toString(arr));
+                }
+
+            }
+        }
+
+    }
